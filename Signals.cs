f5bad04389f9f7e51649a71570857e2e92b54e99
JesseMelon <Jessechambers2000@icloud.com>
2024-03-22 19:34:04 -0400

    cleanup?

--- Signals.cs
+++ Signals.cs
@@ -3,7 +3,7 @@ using System;
 
 
 namespace Project;
-public partial class Signals : Node3D
+public partial class Signals : Node
 {
 
     [Signal] public delegate void TerrainModifiedEventHandler(Vector3 position, bool isAdding);
